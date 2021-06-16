2efdb9524784878bee9714d06393d3bcd504aa05
Kainat <kainat.anjum@emumba.com>
2021-06-16 14:18:08 +0500

    setup fk-testimonial app

---
+++ fk-testimonial/src/App.js
@@ -0,0 +1,11 @@
+import ThankYouPage from './pages/ThankYouPage/ThankYouPage'
+
+function App() {
+  return (
+    <div className="App">
+      <ThankYouPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ fk-testimonial/src/pages/ThankYouPage/ThankYouPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './style.css'
+
+const ThankYouPage = () => {
+    return (
+        <section className="thank-you-page-wrapper">
+            <article className="widget-wrapper">
+                <h2 className="heading">Thank you</h2>
+                <p className="description">We will be in touch if we need anything else</p>
+            </article>
+
+        </section>
+    )
+}
+
+export default ThankYouPage
