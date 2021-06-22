6235a474381165c79eed1a5261eb51e6230008be
Muhammad Arslan Hayat <arslan.hayat@emumba.com>
2021-06-22 09:52:50 +0500

    Set data from query params

--- fk-testimonial/src/components/ClientDetails/ClientDetails.js
+++ fk-testimonial/src/components/ClientDetails/ClientDetails.js
@@ -1,18 +1,22 @@
 import React from "react";
-import { PencilIcon } from "../../assets";
-import './style.css'
+// import { PencilIcon } from "../../assets";
+import { getQueryStringValue } from "../../utils/";
+import "./style.css";
 
 const ClientDetails = () => {
+  const name = getQueryStringValue("name");
+  const email = getQueryStringValue("email");
+  const companyName = getQueryStringValue("company");
   return (
-    <article className='client-details-wrapper'>
-      <article className='client-name-wrapper'>
-        <h3 className="client-name">Brandy Estrada</h3>
-        <span><PencilIcon/></span>
+    <article className="client-details-wrapper">
+      <article className="client-name-wrapper">
+        <h3 className="client-name">{name || "Brandy Estrada"}</h3>
+        {/* <span><PencilIcon/></span> */}
       </article>
-      <p className='client-details'>brandy.estrada@mail.com</p>
-      <p className='client-details'>XYZ Corporation</p>
+      <p className="client-details">{email || "brandy.estrada@mail.com"}</p>
+      <p className="client-details">{companyName || "XYZ Corporation"}</p>
     </article>
   );
 };
 
-export default ClientDetails
+export default ClientDetails;
--- fk-testimonial/src/utils/index.js
+++ fk-testimonial/src/utils/index.js
@@ -1,9 +1,25 @@
 export const convertSecondsToHourMinute = (milliSeconds) => {
-    const sec_num = parseInt(milliSeconds, 10)
-    const hours = Math.floor(sec_num / 3600)
-    const minutes = Math.floor(sec_num / 60) % 60
-    const seconds = sec_num % 60
+  const sec_num = parseInt(milliSeconds, 10);
+  const hours = Math.floor(sec_num / 3600);
+  const minutes = Math.floor(sec_num / 60) % 60;
+  const seconds = sec_num % 60;
 
-    const formattedTime = [hours, minutes, seconds].map(v => (v < 10 ? '0' + v : v)).join(':')
-    return formattedTime
-}
+  const formattedTime = [hours, minutes, seconds]
+    .map((v) => (v < 10 ? "0" + v : v))
+    .join(":");
+  return formattedTime;
+};
+
+export const getQueryStringValue = (key) => {
+  return decodeURIComponent(
+    window.location.search.replace(
+      new RegExp(
+        "^(?:.*[&\\?]" +
+          encodeURIComponent(key).replace(/[\.\+\*]/g, "\\$&") +
+          "(?:\\=([^&]*))?)?.*$",
+        "i"
+      ),
+      "$1"
+    )
+  );
+};
