0129ad3d2340ce831e2df19ba72a5c22060faf4d
Muhammad Arslan Hayat <arslan.hayat@emumba.com>
2021-06-28 12:12:24 +0500

    Add route to serve an app

--- fk-testimonial/server.js
+++ fk-testimonial/server.js
@@ -11,4 +11,8 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
+app.get("/testimonial-poc", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
 app.listen(process.env.PORT || 5000);
