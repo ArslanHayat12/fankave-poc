28dbf23ed9fdd7391bd3e430334f30510fcf77c4
Muhammad Arslan Hayat <arslan.hayat@emumba.com>
2021-06-28 12:42:22 +0500

    Add route to serve an app

--- fk-testimonial/server.js
+++ fk-testimonial/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 app.use(express.static(path.join(__dirname, "build")));
+app.use("/testimonial-poc", express.static(path.join(__dirname, "build")));
 
 app.get("/ping", function (req, res) {
   return res.send("pong");
