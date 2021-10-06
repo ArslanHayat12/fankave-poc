4d336a307d6893bbc678a4b5105dbe6c7f91e990
Sadain Iqrar <sadain.abbasi@gmail.com>
2021-10-06 12:36:24 +0500

    autodesk/users page

--- fk-testimonial/src/Routes.js
+++ fk-testimonial/src/Routes.js
@@ -1,29 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import App from './App'
-import Users from './Users'
 import { waitForAddedNode } from './utils/config'
 
-export default function Routes() {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/users">
-          <Users />
-        </Route>
-        <Route path="/">
-          <App />
-        </Route>
-      </Switch>
-    </Router>
-  )
-}
 waitForAddedNode({
   id: 'fk-creation',
   recursive: false,
   done: (element) => {
-    ReactDOM.render(<Routes />, element)
+    ReactDOM.render(<App />, element)
   },
 })
